3a095e9c805ea7fb0271b8edcf9820584a0927aa
gs3 <doub1e.31415@gmail.com>
2015-09-14 12:37:52 +0900

    Oh!

--- .zsh/peco-sources/vagrant.zsh
+++ .zsh/peco-sources/vagrant.zsh
@@ -3,12 +3,14 @@ function peco_vagrant() {
                     peco | \
                     awk '{print $2}')
 
-  vagrant_command=$(vagrant list-commands | \
-                    awk 'NR >= 4' | \
-                    peco | \
-                    awk '{print $1}')
-  BUFFER="vagrant ${vagrant_command} ${vagrant_host}"
-  CURSOR=$#BUFFER
-  zle -R -c
+  if [ -n "$vagrant_host" ]; then
+    vagrant_command=$(vagrant list-commands | \
+      awk 'NR >= 4' | \
+      peco | \
+      awk '{print $1}')
+    BUFFER="vagrant ${vagrant_command} ${vagrant_host}"
+    CURSOR=$#BUFFER
+    zle -R -c
+  fi
 }
 zle -N peco_vagrant
