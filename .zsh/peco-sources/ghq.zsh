b006ac516d2eefa5be612ec1df531c1926128e1d
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2019-04-03 14:25:35 +0900

    [zshrc] Choose a repo from not fully path

--- .zsh/peco-sources/ghq.zsh
+++ .zsh/peco-sources/ghq.zsh
@@ -1,9 +1,9 @@
 setopt hist_ignore_all_dups
 
 function peco_select_ghq() {
-  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  local selected_dir=$(ghq list | peco --query "$LBUFFER")
   if [ -n "$selected_dir" ]; then
-    BUFFER="cd ${selected_dir}"
+    BUFFER="cd $GHQ_ROOT/${selected_dir}"
     zle accept-line
   fi
 }
